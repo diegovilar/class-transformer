11d8b994563391a17f75bf91550bf4d47d95ed71
Diego Vilar <diegovilar@gmail.com>
2018-03-05 12:09:53 -0300

    v0.1.9

--- src/metadata/MetadataStorage.ts
+++ src/metadata/MetadataStorage.ts
@@ -29,7 +29,7 @@ export class MetadataStorage {
 
     constructor() {
 
-        console.warn(`CLASS-TRANSFORMER: new MetadataStorage()`);
+        // console.warn(`CLASS-TRANSFORMER: new MetadataStorage()`);
 
     }
 
